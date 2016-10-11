3bf1cdf0180bd647767904758040d363c838fb1d
SHUBHAM KUMAR <shubhamdec16@gmail.com>
2016-10-11 20:49:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,24 @@
-console.log('Loaded!');
-//change the text of main-text div
-var element = document.getElementById('main-text');
-element.innerHTML = 'New Value';
-//move image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
- var interval = setInterval(moveRight, 10);  
+//counter code
+var button = document.getelEmentById('counter');
+
+button.onclick = function (){
+  
+  //create the request object.
+  var request = XMLHttpRequest();
+  //capture the response and store it in a variable.
+  request.onreadystatechange = function(){
+   if(request.readyState === XMLHttpRequest.DONE){
+       //take some action 
+       if(request.status === 200){
+           var counter = request.responseText;
+            var span = document.getElementById('count');
+           span.innerHTML = counter.toString();
+       }
+   }  
+   //not done yet
+  };
+  //make the request
+  request.open('GET','http://b315045.imad.hasura-app.io/counter',true);
+  request.send(null);
+ 
 };
